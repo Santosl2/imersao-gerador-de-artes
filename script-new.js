c52e127d63f3569f02c340d0bab0b13f567cbd5a
Santosl2 <mfilype2019@gmail.com>
2024-11-05 22:53:39 -0300

    feat

---
+++ script-new.js
@@ -0,0 +1,160 @@
+const canvas = document.getElementById("canvas");
+const img = document.getElementById("img");
+const generateButton = document.getElementById("generate");
+const form = document.querySelector("form");
+const enderecoInput = document.querySelector("[name=endereco]");
+const bairroInput = document.querySelector("[name=bairro]");
+const numeroInput = document.querySelector("[name=numero]");
+const tipoInput = document.querySelector("[name=tipo]");
+
+async function getConfigJson() {
+  const data = await fetch(`./config.json`, {
+    method: "GET",
+  });
+
+  return await data.json();
+}
+
+async function getImageBlob(image) {
+  const data = await fetch(`./images/${image}`, {
+    method: "GET",
+  });
+
+  return await data.blob();
+}
+
+async function loadFonts() {
+  const font = new FontFace(
+    "MonumentRegular",
+    "url(./fonts/MonumentExtended-Regular.otf)"
+  );
+  await font.load();
+  document.fonts.add(font);
+}
+
+const coords = {
+  telao: {
+    rua: {
+      x: 161.1,
+      y: 965,
+    },
+    bairro: {
+      x: 161.1,
+      y: 1007,
+    },
+  },
+  story: {
+    rua: {
+      x: 161.1,
+      y: 965,
+    },
+    bairro: {
+      x: 161.1,
+      y: 1007,
+    },
+  },
+};
+
+async function loadImages() {
+  await loadFonts();
+
+  const type = tipoInput.value;
+
+  const telao = getImageBlob(`${type}/telao.png`);
+  const story = getImageBlob(`${type}/story.png`);
+
+  generateButton.textContent = "Gerando artes...";
+  generateButton.disabled = true;
+  const promises = Promise.all([telao, story]);
+  const [telaoBlob, storyBlob] = await promises;
+
+  // const zip = zipData();
+  const baseTelaoImage = new Image();
+  baseTelaoImage.src = URL.createObjectURL(telaoBlob);
+
+  baseTelaoImage.onload = function () {
+    canvas.width = baseTelaoImage.width;
+    canvas.height = baseTelaoImage.height;
+    drawData.call(this, { type: "telao", img: baseTelaoImage });
+
+    // zip.addFileToZip("telao.png", canvas.toDataURL());
+    createDownloadButton(canvas, "telao");
+  };
+
+  const baseStoryImage = new Image();
+  baseStoryImage.src = URL.createObjectURL(storyBlob);
+  baseStoryImage.onload = function () {
+    canvas.width = baseStoryImage.width;
+    canvas.height = baseStoryImage.height;
+    drawData.call(this, { type: "story", img: baseStoryImage });
+    // zip.addFileToZip("story.png", canvas.toDataURL());
+    createDownloadButton(canvas, "story");
+  };
+
+  generateButton.textContent = "Gerar artes";
+  generateButton.disabled = false;
+  // await zip.generateZip();
+}
+
+function createDownloadButton(canvas, name) {
+  const downloadButton = document.createElement("a");
+  downloadButton.innerText = `Baixar arte ${name}`;
+  downloadButton.href = canvas.toDataURL();
+  downloadButton.download = `${name}-${new Date().getTime()}.png`;
+  form.appendChild(downloadButton);
+}
+
+async function drawData({ type = "telao", img }) {
+  const coords = await getConfigJson();
+  const department = tipoInput.value;
+
+  const { rua: ruaCoords, bairro: bairroCoords } =
+    coords[department][type] || {};
+
+  if (!ruaCoords || !bairroCoords) {
+    console.error("Oops, erro ao capturar coordenadas");
+    return;
+  }
+
+  const ctx = canvas.getContext("2d");
+  ctx.drawImage(img, 0, 0);
+  ctx.font = "41.66px MonumentRegular";
+  ctx.fillStyle = "white";
+
+  const endereco = enderecoInput.value.replace(/rua/gi, "").trim();
+  const bairro = bairroInput.value;
+
+  const enderecoFormatado = `R. ${endereco}, ${numeroInput.value}`;
+
+  if (type === "story") {
+    function measureTextSize(txt) {
+      const textWidth = ctx.measureText(txt).width;
+      return textWidth / 2;
+    }
+
+    const textWidth = measureTextSize(enderecoFormatado);
+    const textWidth2 = measureTextSize(bairro);
+
+    const x = this.width / 2;
+    ctx.fillText(enderecoFormatado, x - textWidth, this.height - 293);
+    ctx.fillText(bairro, x - textWidth2, this.height - 253);
+  } else {
+    ctx.fillText(enderecoFormatado, ruaCoords.x, ruaCoords.y);
+    ctx.fillText(bairro, bairroCoords.x, bairroCoords.y);
+  }
+}
+
+function zipData() {
+  const zip = new JSZip();
+
+  function addFileToZip(name, data) {
+    zip.file(name, data, { base64: true });
+  }
+
+  async function generateZip() {
+    const content = await zip.generateAsync({ type: "blob" });
+    saveAs(content, "Imersão.zip");
+  }
+
+  return { addFileToZip, generateZip };
+}
